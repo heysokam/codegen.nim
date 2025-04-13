5b010a06f483ea13e64ab5df8d999caff00b082f
sOkam <7308253+heysokam@users.noreply.github.com>
2025-04-13 13:18:17 +0200

    bld: Add confy builder for tests

---
+++ build.nim
@@ -0,0 +1,17 @@
+#:______________________________________________________________________
+#  nim.gen  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
+#:______________________________________________________________________
+import confy
+import std/os
+import std/strutils
+
+Program.new("entry.nim").build.run
+
+
+#_______________________________________
+# @section Run all UnitTest files
+#_____________________________
+if dirExists("./bin/.tests"): removeDir("./bin/.tests")
+for testFile in os.walkDirRec("./src", yieldFilter= {pcFile}, relative= true):
+  if not testFile.endsWith("_test.nim"): continue
+  Program.new(testFile, sub="tests").build.run
