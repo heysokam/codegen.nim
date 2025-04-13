7fc9a74fd1a7df7052968569ac6583a66f2711f6
sOkam <7308253+heysokam@users.noreply.github.com>
2025-04-13 13:18:37 +0200

    bld: Dummy entry point file

---
+++ src/entry.nim
@@ -0,0 +1,8 @@
+#:______________________________________________________________________
+#  nim.gen  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
+#:______________________________________________________________________
+# @deps compiler
+import "$nim"/compiler/[ ast, parser, idents, options, lineinfos, msgs, pathutils, syntaxes ]
+from   "$nim"/compiler/renderer import renderTree
+
+echo "Hello Entry"
