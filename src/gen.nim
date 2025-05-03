2e625d286b554314f6e4992e2366cf30d4d5c7c9
sOkam <7308253+heysokam@users.noreply.github.com>
2025-05-03 09:11:42 +0200

    new: Entry point generates toplevel variables/procs randomly

--- src/gen.nim
+++ src/gen.nim
@@ -4,24 +4,41 @@
 # @deps compiler
 import "$nim"/compiler/[ ast, options, lineinfos, msgs, pathutils ]
 from   "$nim"/compiler/renderer import renderTree, renderNoComments, renderNoPragmas
-
-# Import the modules
+# @deps generator
+import ./random
 import ./gen/procs
+import ./gen/statement/variable
 
-proc root *(path :string) :tuple[node: PNode, info: TLineInfo]=
+type RootData * = tuple[node :PNode, info :TLineInfo]
+proc render *(
+    root        : RootData;
+    allowBlocks : bool = true;
+  ) :string=
+  # FIX: Remove const blocks
+  renderTree(root.node, {renderNoComments, renderNoPragmas})
+
+proc root *(path :string) :RootData=
   let config  = newConfigRef()
   let absPath = AbsoluteFile(path)
   let info    = newLineInfo(config, absPath, 0, 0)
   let node    = newNodeI(nkStmtList, info)
   return (node:node, info:info)
 
-proc nim *(path: string = "generated.nim"): string =
-  ## Main function to generate random Nim code
+proc nim *(
+    path        : string = "generated.nim";
+    allowBlocks : bool   = false;
+  ) :string=
+  ## @descr Generates random Nim code
+  ## @arg allowBlocks (default: false) Will allow generating let/var/const/etc blocks when true.
   let root = gen.root(path)
 
-  # Generate a random procedure
-  root.node.add procs.random(root.info)
+  # Generate a random amount of TopLevel statements
+  for _ in 0..<random.integer(128):
+    case random.integer(1):
+    of 0: root.node.add variable.random(root.info)
+    of 1: root.node.add procs.random(root.info)
+    else:discard
 
   # Convert the AST to a string
-  return renderTree(root.node, {renderNoComments, renderNoPragmas})
+  return gen.render(root, allowBlocks)
 
