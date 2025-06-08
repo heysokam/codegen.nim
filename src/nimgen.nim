e9892143ec546f059e3a1cd2681551dff7c0900b
sOkam <7308253+heysokam@users.noreply.github.com>
2025-06-08 22:31:17 +0200

    chg: Entry point initializes the seed and reports it to cli

--- src/nimgen.nim
+++ src/nimgen.nim
@@ -3,30 +3,43 @@
 #:______________________________________________________________________
 # @deps std
 from std/os import nil
+from std/strutils import join, toHex
+from std/strformat import `&`
 # @deps nim.gen
+import ./random
 import ./gen
+
+#_______________________________________
+# @section nim.gen Logging
+#_____________________________
+func info *(msg :varargs[string, `$`]) :void=
+  debugEcho "[nim.gen] " & msg.join(" ")
 
 
 #_______________________________________
 # @section nim.gen Entry Point
 #_____________________________
-proc run=
-  let args   = os.commandLineParams()
+proc run (args :seq[string])=
   doAssert args.len in {2,3}
   let choice = args[0]
   let file   = args[1]
   let dir    = os.splitFile(file).dir
-  # let seed   = if args.len > 2: args[2] else: random.integer().toHex()
+  let seed   = if args.len > 2: args[2] else: random.integer().toHex()
+  random.init(seed)
   # Validate Arguments
   doAssert choice in ["all", "variable", "proc"]
   doAssert not os.fileExists(file)
+  # Report to CLI
+  info(&"Generating {choice} code at {file} with seed {seed}")
+  # Run the generator
   let code = case choice # FIX: Pass allowBlocks option from cli
   of "variable" : gen.variable()
   of "proc"     : gen.procs()
   else          : gen.nim()
+  # Write to the file
   if not os.dirExists(dir): os.createDir dir
   file.writeFile(code)
-  echo code
+  when defined(debug): echo code
 #___________________
-when isMainModule: run()
+when isMainModule: nimgen.run(os.commandLineParams())
 
